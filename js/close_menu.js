cdf096c18ab6f9df1746ed40236b967f0215065e
Jennifer Bester <besterjennifer5@gmail.com>
2017-08-12 20:38:49 +0200

    Add travelbook sidebar

--- js/close_menu.js
+++ js/close_menu.js
@@ -1,14 +1,3 @@
-$(document).ready(function(){
-	$(".sidebar").addClass("hide");
-	$(".itenerary").addClass("opacity1");
-});
-	
-$(".itenerary").click(function(){
-	$(".sidebar").removeClass("hide");
-	$(".itenerary").removeClass("opacity1");
-});
-	
-$(".close").click(function(){
-	$(".sidebar").addClass("hide");
-	$(".itenerary").addClass("opacity1");
+$(".toggle-travelbook").click(function(){
+	$(".sidebar").toggle();
 });
