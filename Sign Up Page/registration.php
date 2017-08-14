3c86d7c03847c3d0b7cd8ba4f5776e3d50d65760
Unknown <24522988@student.g.nwu.ac.za>
2017-08-14 12:54:21 +0200

    /website:
    Vernadering aan die files van html na php

---
+++ Sign Up Page/registration.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Travel Buddy</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,500,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+		<link href="../css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+		<!-- NAVBAR -->
+		<nav class="navbar">
+				<a href = "../home.html" class="logo logo-small-light">Travel Buddy</a>
+				<div>
+					<a class="btn btn-outline-secondary btn-sm" href="../Login Page/login.html">Login</a>
+				</div>
+		</nav>
+		<!-- NAVBAR END -->
+		<div class="fullscreen-section">
+				<div class="card">
+				<p class="logo logo-small-dark">Travel Buddy</p>
+				<h2>Sign up</h2>
+				<p>Please fill in the following details to start your adventure!</p>
+				<form>
+					<div class="form-group">
+						<p>First Name:</p>
+						<input id = "fName" type = "text" class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<p>Last Name:</p>
+						<input id = "lName" type = "text" class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<p>E-Mail: </p>
+						<input id = "email" type = "email" class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<p>Contact Number: </p>
+						<input id = "number" type = "int" class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<p>Username: </p>
+						<input id = "uName" type = "text" class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<p>Password: </p>
+						<input id = "pWord" type = "password" class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<p>Re-enter Password: </p>
+						<input id = "pWord2" type = "password" class="form-control"></input>
+					</div>
+					<div class="form-group">
+						<p>Where do you stay? </p>
+						<input id = "town" type = "text" class="form-control"></input>
+					</div>
+					<a href="../Landing Page/landing-page.html" class = "btn btn-primary btn-submit float-right">Submit</a>
+					<div class = "buttons">
+						<a href = "../home.html">Back</a>&emsp;
+					</div>
+				</form>
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	</body>
+</html>
