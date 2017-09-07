4f7372aefdea3fa65ad1ad57309f06bbb825daa4
Dehan <dehanduvenhage@gmail.com>
2017-09-07 16:11:14 +0200

    New TravelBook UI Design
    Created new travelbook UI

---
+++ Mobile_App/MyApplication/app/src/main/java/travel_buddyapp/travelbuddyapp/NewTravelbook_Activity.java
@@ -0,0 +1,13 @@
+package travel_buddyapp.travelbuddyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTravelbook_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_travelbook_);
+    }
+}
