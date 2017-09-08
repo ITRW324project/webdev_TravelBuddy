39818ed331de589dc90f51d420c89caf2649d3ec
Stephan Fourie <scfourienwu@gmail.com>
2017-09-08 21:21:55 +0200

    webservice update
    improved efficiency

--- locationsTest.php
+++ locationsTest.php
@@ -7,62 +7,58 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $f_loc_name = $_POST['end'];
     $w_loc_name = $_POST['waypoints']
 
-    $sql_query_start = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$s_loc_name."'";
-    $result_loc_s = mysqli_query($db_travel,$sql_query_start);
-    $row = mysqli_fetch_array($result_loc_s,MYSQLI_ASSOC);
-    $active = $row['active'];
-    $count = mysqli_num_rows($result_loc_s);
-
-    //$sql_query_p = "SELECT Plan_ID FROM Plan WHERE Plan_Name = '".$plan_name."'";
-    //$result_plan = mysqli_query($db_travel,$sql_query_p);
-
-    if($count == 0)
+    function count($name)
     {
-        $sql_query_loc = "INSERT INTO Locations (Loc_Name) VALUES ('".$s_loc_name."')";
-        mysqli_query($db_travel,$sql_query_loc);
-
-        $sql_query_start = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$s_loc_name."'";
-        $result_loc_s = mysqli_query($db_travel,$sql_query_start);
+      $sql_query = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$name."'";
+      $result_loc = mysqli_query($db_travel,$sql_query);
+      $row = mysqli_fetch_array($result_loc,MYSQLI_ASSOC);
+      $active = $row['active'];
+      $count = mysqli_num_rows($result_loc);
+      return $count;
     }
 
-    $sql_query_end = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$f_loc_name."'";
-    $result_loc_f = mysqli_query($db_travel,$sql_query_end);
-    $row = mysqli_fetch_array($result_loc_f,MYSQLI_ASSOC);
-    $active = $row['active'];
-    $count = mysqli_num_rows($result_loc_f);
-
-    if($count == 0)
+    function addLocation($name)
     {
-        $sql_query_loc = "INSERT INTO Locations (Loc_Name) VALUES ('".$f_loc_name."')";
-        mysqli_query($db_travel,$sql_query_loc);
+      $sql_query_loc = "INSERT INTO Locations (Loc_Name) VALUES ('".$name."')";
+      mysqli_query($db_travel,$sql_query_loc);
 
-        $sql_query_end = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$f_loc_name."'";
-        $result_loc_f = mysqli_query($db_travel,$sql_query_end);
+      $sql_query = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$name."'";
+      $result_loc = mysqli_query($db_travel,$sql_query);
+
+      return $result_loc;
     }
 
-    $sql_query_loc_s = "INSERT INTO Plan_Loc (Plan_ID, Loc_ID, Seq_Value) VALUES ('". ."', '".$result_loc_s."', '".'start'."')";
+    /*$sql_query_p = "SELECT Plan_ID FROM Plan WHERE Plan_Name = '".$plan_name."'";
+    $result_plan = mysqli_query($db_travel,$sql_query_p);*/
+
+    $countS = count($s_loc_name);
+    $countF = count($f_loc_name);
+
+    if($countS == 0)
+    {
+        $result_loc_s  = addLocation($s_loc_name);
+    }
+
+    if($countF == 0)
+    {
+        $result_loc_f  = addLocation($f_loc_name);
+    }
+
+    $sql_query_loc_s = "INSERT INTO Plan_Loc (Plan_ID, Loc_ID, Seq_Value) VALUES ('". ."', '".$result_loc_s."', 'start')";
     mysqli_query($db_travel,$sql_query_loc_s);
-    $sql_query_loc_f = "INSERT INTO Plan_Loc (Plan_ID, Loc_ID, Seq_Value) VALUES ('". ."', '".$result_loc_f."', '".'end'."')";
+    $sql_query_loc_f = "INSERT INTO Plan_Loc (Plan_ID, Loc_ID, Seq_Value) VALUES ('". ."', '".$result_loc_f."', 'end')";
     mysqli_query($db_travel,$sql_query_loc_f);
 
     foreach ($w_loc_name as $selectedOption)
     {
-        $sql_query_way = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$selectedOption."'";
-        $result_loc_w = mysqli_query($db_travel,$sql_query_way);
-        $row = mysqli_fetch_array($result_loc_w,MYSQLI_ASSOC);
-        $active = $row['active'];
-        $count = mysqli_num_rows($result_loc_w);
+        $countW = count($selectedOption);
 
         if($count == 0)
         {
-            $sql_query_loc = "INSERT INTO Locations (Loc_Name) VALUES ('".$selectedOption."')";
-            mysqli_query($db_travel,$sql_query_loc);
-
-            $sql_query_way = "SELECT Loc_ID FROM Locations WHERE Loc_Name = '".$selectedOption."'";
-            $result_loc_way = mysqli_query($db_travel,$sql_query_way);
+            $result_loc_w  = addLocation($selectedOption);
         }
 
-        $sql_query_loc_w = "INSERT INTO Plan_Loc (Plan_ID, Loc_ID, Seq_Value) VALUES ('". ."', '".$result_loc_way."', '".'waypoint'."')";
+        $sql_query_loc_w = "INSERT INTO Plan_Loc (Plan_ID, Loc_ID, Seq_Value) VALUES ('". ."', '".$result_loc_w."', waypoint')";
         mysqli_query($db_travel,$sql_query_loc_w);
     }
 }
