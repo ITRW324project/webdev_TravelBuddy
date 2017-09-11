99f6a78c79c7ebeaa979e66f653fa911f59a04a8
Dehan <dehanduvenhage@gmail.com>
2017-09-11 17:48:22 +0200

    Seach Back Button test

--- Mobile_App/MyApplication/app/src/main/java/travel_buddyapp/travelbuddyapp/Search_Activity.java
+++ Mobile_App/MyApplication/app/src/main/java/travel_buddyapp/travelbuddyapp/Search_Activity.java
@@ -1,13 +1,32 @@
 package travel_buddyapp.travelbuddyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Search_Activity extends AppCompatActivity {
+
+   public Button but1;
+
+    public void init(){
+        but1= (Button)findViewById(R.id.bBack_Search);
+        but1.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                Intent toy = new Intent(Search_Activity.this,WelcomeActivity.class);
+                startActivity(toy);
+            }
+        });
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_);
+        init();
+
     }
 }
