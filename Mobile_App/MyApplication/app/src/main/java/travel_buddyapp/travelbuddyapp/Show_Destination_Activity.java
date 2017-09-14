d4d26ca4b4583cf367416b3e174794188cdb7446
Jennifer Bester <besterjennifer5@gmail.com>
2017-09-14 22:27:47 +0200

    Destination View Mock Data
    Struggle with Maps Integration, therefore crated mock data to show how sould look

---
+++ Mobile_App/MyApplication/app/src/main/java/travel_buddyapp/travelbuddyapp/Show_Destination_Activity.java
@@ -0,0 +1,13 @@
+package travel_buddyapp.travelbuddyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Show_Destination_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show__destination_);
+    }
+}
